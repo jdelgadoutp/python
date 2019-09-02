9d24bb9563827ff42cac6341237db46ea086490e
jdelgadoutp <j.delgado@utp.edu.co>
2019-09-01 21:51:37 -0500

    cambios en vectores.

--- figuras.py
+++ figuras.py
@@ -21,18 +21,17 @@ windows=pygame.display.set_mode((800,600))
 pygame.draw.rect(windows,(23,251,205),(0,0,50,30))
 pygame.draw.line(windows,(23,251,205),(100,300),(700,300),2)
 pygame.draw.line(windows,(23,251,205),(400,100),(400,500),2)
-pygame.draw.line(windows,(255,27,23),(orix,oriy),(orix + x,oriy - y),3)
-oryxtemp = orix + x
-oriytemp = oriy - y
-x=Vx(120,36)
-y=Vy(120,36)
-pygame.draw.line(windows,(3,236,26),(oryxtemp,oriytemp),(oryxtemp + x,oriytemp - y),3)
+pygame.draw.line(windows,(255,27,23),(orix,oriy),(orix + Vx(100,20),oriy - Vy(100,20)),3)
 
-oryxtempa = oryxtemp + x
-oriytempy = oriytemp - y
-x=Vx(-80,30)
-y=Vy(80,30)
-pygame.draw.line(windows,(3,236,26),(oryxtempa,oriytempy),(oryxtempa + x,oriytempy - y),3)
+orix=orix + Vx(100,20)
+oriy=oriy - Vy(100,20)
+
+pygame.draw.line(windows,(3,236,26),(orix,oriy),(orix + Vx(120,36),oriy - Vy(120,36)),3)
+
+orix=orix + Vx(120,36)
+oriy=oriy - Vy(120,36)
+
+pygame.draw.line(windows,(3,236,26),(orix,oriy),(orix + Vx(-80,30),oriy - Vy(80,30)),3)
 pygame.display.update()
 
 time.sleep(9.8)
